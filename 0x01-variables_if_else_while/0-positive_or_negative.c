52a9a8b1e50bc36357a445ba06a4751ba2ffa183
peternmacharia <peternmacharia@outlook.com>
2022-03-11 22:50:07 -0800

    Task #0 second commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,3 +4,9 @@ if(i > 0){printf ("is positive");}
 if(i == 0){printf ("is zero");}
 if(i < 0){printf("is negative");}
 
+
+int i = 0;
+if(i > 0) {print ("is positive");}
+if(i == 0) {print ("is zero");}
+if(i < 0) {print ("is negative");}
+
