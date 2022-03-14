599420c36db7de2b7052b4e425b30b7d468daac1
peternmacharia <peternmacharia@outlook.com>
2022-03-14 13:33:56 -0700

    #0 Task fourth commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,9 +15,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%d is positive \n");
+		printf("n is positive \n");
 	if (n == 0)
-		printf("%d is zero \n");
+		printf("n is zero \n");
 	if (n < 0)
-		printf("%d is negative \n");
+		printf("n is negative \n");
 	return (0);
