72d5058ea742c97ae1139e960c316bc2b28c54a0
peternmacharia <peternmacharia@outlook.com>
2022-03-18 14:59:31 -0700

    #1 Task #14 commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
-	if (n == 0 || n !> 5)
+	if (n =< 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
