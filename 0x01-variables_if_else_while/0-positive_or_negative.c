54beadd2f9e7a665ed7e8ea6c03c9918f5319157
peternmacharia <peternmacharia@outlook.com>
2022-03-18 06:53:15 -0700

    #1 Task fourth commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 /* betty style doc for function main goes there */
 int main(void)
 {
