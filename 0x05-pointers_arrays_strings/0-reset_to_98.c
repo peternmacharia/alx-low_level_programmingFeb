6962dfac4023ce97a3e034d8c31c21cf8763254c
peternmacharia <peternmacharia@outlook.com>
2022-03-22 15:36:33 -0700

    #0 Task #6 commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
- *  * main - check the code 
- *   *
- *    * Return: Always 0.
- *     */
+ * main - check the code 
+ *
+ * Return: Always 0.
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
