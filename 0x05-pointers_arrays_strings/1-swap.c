4309e04612dbbce9cb3d34da72174679c94c993c
peternmacharia <peternmacharia@outlook.com>
2022-03-22 16:26:30 -0700

    #1 Task commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_in - Entry point
+ *
+ * @a - first pointer
+ * @b - second pointer
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 98;
+	*b = 42;
+}
