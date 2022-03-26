39b1073c1a870449913c4f11b706865d3143f365
peternmacharia <peternmacharia@outlook.com>
2022-03-26 11:32:31 +0300

    String Concatination #11 commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,25 +5,18 @@
  *
  * @dest: Variable String
  * @src: Variable String
+ *
  * Return: 0
  */
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-	i = 0;
-	while(dest[i] != '\0')
-	{
-		i++;
-	}
+	i = -1;
+	while(dest[++1]);
 
 	j = 0;
-	while(src[j] !='\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
+	while(dest[i++] = src[j++]);
+	printf("%s", dest);
 	return (0);
 }
