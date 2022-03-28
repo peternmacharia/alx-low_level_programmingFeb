06ccf626a6bbe324a8db7b7dd5b49f76f0061818
peternmacharia <peternmacharia@outlook.com>
2022-03-29 00:19:16 +0300

    String Substring commit #2

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,11 +11,21 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	while (*accept)
+	int i, j;
+	int c = 0;
+
+	for (i = 0; s[i] !='\0'; i++)
 	{
-		if (s != accept)
-			s++;
+		if (s[i] != 32)
+		{
+			for (j = 0; accept[j] != '\0'; j++)
+			{
+				if (s[i] == accept[j])
+					c++;
+			}
+		}
 		else
-			return (s);
+			return (c);
 	}
+	return (c);
 }
