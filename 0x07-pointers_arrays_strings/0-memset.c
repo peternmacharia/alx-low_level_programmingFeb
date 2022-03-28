adca46887fd8df8a8b9e6367210a2f656a295ebf
peternmacharia <peternmacharia@outlook.com>
2022-03-28 23:07:48 +0300

    Memeset #3 commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,19 +2,19 @@
 #include <stdio.h>
 
 /**
- * memset - Function entry point
+ * memset - Fills memory with constant type
  *
- * @s: string variable
- * @b: string variable
- * @n: int variable
+ * @s: pointer to memory area to fill
+ * @b: constant byte
+ * @n: the number of bytes to fill
  *
- * Return: Nothing
+ * Return: a pointer to the memory area @s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
