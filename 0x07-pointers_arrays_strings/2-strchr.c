27bc46e8d477c013cbc7c0f9e33de5c5ca1031c0
peternmacharia <peternmacharia@outlook.com>
2022-03-28 23:41:20 +0300

    String Character commit #0

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - function that locates a character in a string
+ *
+ * @s: pointer to a string
+ * @c: character string
+ *
+ * Return: pointer to the first string character
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; i <= c[i], i++)
+	{
+		if (c == "c")
+		{
+			return (c);
+		}
+		else
+		{
+			return ("NULL");
+		}
+	}
+}
