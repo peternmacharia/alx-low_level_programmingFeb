4f62e8ad7854d0d5d88fbf02cdf5a8ad26825334
peternmacharia <peternmacharia@outlook.com>
2022-03-28 23:56:41 +0300

    String Character #3 commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,16 +13,12 @@ char *_strchr(char *s, char c)
 {
 	while (*s)
 	{
-		if (*s == c)
-		{
-			return (s);
-		}
-		else
-		{
+		if (*s != c)
 			s++;
-		}
+		else
+			return (s);
 	}
-	if (c == "\0")
+	if (c == '\0')
 		return (s);
 
 	return (NULL);
