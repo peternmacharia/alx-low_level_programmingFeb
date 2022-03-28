3c2cc90b5e802e38920c084b4b593fb2cd9300d3
peternmacharia <peternmacharia@outlook.com>
2022-03-29 02:08:30 +0300

    Double Pointer commit #0

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_string - function that sets the value of a pointer to char
+ *
+ * @s: double pointer
+ * @to: pointer to the character string
+ *
+ * Return: o
+ */
+void set_string(char **s, char *to)
+{
+	s = to;
+}
